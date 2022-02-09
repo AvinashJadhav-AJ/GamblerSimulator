2e48ac4a4432fbd3df70a47cb87b00c4a28844f3
Avinash <aj912873@gmail.com>
2022-02-09 13:15:06 +0530

    Added: Player Stake per Day And Bet

--- src/com/BridgeLabz/java/GamblerSimulator.java
+++ src/com/BridgeLabz/java/GamblerSimulator.java
@@ -1,6 +1,8 @@
 package com.BridgeLabz.java;
 
 public class GamblerSimulator {
+	static int stake = 100;
+	static int Bet = 1;
 	public static void main(String[] Args ) {
 		System.out.println("Welcome to Gambler Simulator");
 	}
