2f6feafb0924043ca8c01bfef59beef0dae58b74
Avinash <aj912873@gmail.com>
2022-02-09 13:17:57 +0530

    Added: Player First Bet

--- src/com/BridgeLabz/java/GamblerSimulator.java
+++ src/com/BridgeLabz/java/GamblerSimulator.java
@@ -1,10 +1,21 @@
 package com.BridgeLabz.java;
 
 public class GamblerSimulator {
-	static int stake = 100;
-	static int Bet = 1;
 	public static void main(String[] Args ) {
-		System.out.println("Welcome to Gambler Simulator");
+		int stake = 100;
+		int bet =1;
+		System.out.println("Weclome To Gambler Simulator");
+		double Betting = Math.floor(Math.random() * 100) % 2;
+		if ( Betting == bet ) {
+			stake ++;
+			System.out.println("Gambler is Winning $1");
+			System.out.println("stake= " + stake);
+		}
+		else {
+			stake --;
+			System.out.println("Gambler is loseing $1");
+			
+		}
 	}
 
 }
